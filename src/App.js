20d64e555af6e0a500e749c58fd91baef70225dd
diegol14 <diegolpereira1414@gmail.com>
2024-09-12 20:33:28 +0200

    firstOne

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+//import { useState } from 'react';
+import React, { useState } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [resultado, setResultado] = useState(null);
+
+  const title = <h1 className='titleApp'>Hola trolos!</h1>
+
+
+  const pushedButton = () => {
+    const result = sum(7, 5);
+    setResultado(result);
+  };
+
+  return <div> <div>{title}</div>
+    <div className='App'><button onClick={pushedButton}>PushMe!!</button></div>
+    <div className='App'>{resultado != null &&<h2>El resultado es: {resultado}</h2>}</div>
+  </div>
+}
+
+function sum(a, b) {
+  return a + b;
 }
 
 export default App;
